94b5abbf5f6ccb543b135503437b352a938674b8
DharaniTharan06 <duraisamydharanitharan@gmail.com>
2025-08-29 19:24:43 +0530

    Update demo

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <h1 className='bg-green-200 rounded'>Hello world!</h1>
-    </>
+    <div className='w-full h-screen inset-0 fixed' style={{backgroundColor: "lavenderblush"}}>
+      <div className='flex flex-wrap ' style={{backgroundColor:"grey"}}>
+        <div></div>
+      </div>
+    </div>
   )
 }
 
